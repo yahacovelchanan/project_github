af9a32087f52f26f7aec11a0d1dc23b4554dc8dd
Haim Vales <haimvales@gmail.com>
2026-02-24 15:52:44 +0200

    meseg

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Aside from './comps/Aside'
 import Heder from './comps/Heder'
+import Meseg from './comps/Meseg'
 
 
 export default function App() {
@@ -8,6 +9,7 @@ export default function App() {
     <div className='flex-col gap-5 border'>
         <Heder />
         <Aside />
+        <Meseg />
     </div>
   )
 }
