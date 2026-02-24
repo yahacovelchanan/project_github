10e58cbda78206832155f43401cf4a76f2c404ba
Haim Vales <haimvales@gmail.com>
2026-02-24 15:17:34 +0200

    size

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Timer from './comps/timer'
 import Bombs from './comps/Bombs'
+import SizeBoard from './comps/sizeBoard'
 
 export default function App() {
   return (
     <div>
         <Timer/>
         <Bombs />
+        <SizeBoard />
     </div>
   )
 }
